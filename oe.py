f42146bbfe7d6a0f592a982c1904da496fdc884f
musfi64 <Asher786@yahoo.com>
2024-12-03 05:02:28 -0600

    fxgh

---
+++ oe.py
@@ -0,0 +1,6 @@
+m=int(input("Enter a number"))
+print(m)
+if m%2==0:
+    print("even")
+else:
+    print("odd")
---
+++ pl.py
@@ -0,0 +1,8 @@
+cp=int(input("Enter a number"))
+sp=int(input("Enter a number"))
+profit=sp-cp
+print(profit)
+if sp > cp:
+    print("profit")
+else:
+    print("lose")
